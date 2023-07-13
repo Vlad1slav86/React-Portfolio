439a439fcbc3422e9a43e2f6bfdcfa5b08654e53
VladM <vladkb@yahoo.com>
2023-07-13 11:26:42 -0700

    updated resume

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    // Prepare the form data
+    const formData = {
+      name,
+      email,
+      message,
+    };
+  
+    // Make an API call to submit the form data
+    fetch('/api/submit-contact-form', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Form submission successful
+          setFormSubmitted(true);
+        } else {
+          // Form submission failed
+          console.log('Form submission failed.');
+        }
+      })
+      .catch((error) => {
+        console.log('An error occurred while submitting the form:', error);
+      });
+  };
+  
+
   return (
-    <section className="contact-section">
-        <h2>Contact Me</h2>
-      <div className="contact-content">
-        
-        <ul>
-          <li><strong>Phone:</strong> 323 695 8943</li>
-          <li><strong>Email:</strong> vladkb@yahoo.com</li>
-          <li><strong>LinkedIn:</strong></li>
-        </ul>
-      </div>
+    <section id="contact">
+      <h2>Contact Me</h2>
+      {!formSubmitted ? (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div className="form-submitted-message">
+          Thank you for your message! I'll get back to you soon.
+        </div>
+      )}
     </section>
   );
 };
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Resume.css';
 
 const Resume = () => {
+  const proficiencies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Express.js', 'MongoDB'];
+
   return (
-    <section>
-      Resume content goes here 
+    <section id="resume">
+      <h2>Resume</h2>
+      <div>
+        <h3>Download Resume</h3>
+        <a href="/path/to/resume.pdf" download>Download PDF</a>
+      </div>
+      <div>
+        <h3>Proficiencies</h3>
+        <ul>
+          {proficiencies.map((proficiency, index) => (
+            <li key={index}>{proficiency}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
